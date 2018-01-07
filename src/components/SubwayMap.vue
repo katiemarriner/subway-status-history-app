b73f0c4c09d9bb88a5b60e3610abb781ca185e69
katiemarriner <kemarriner@gmail.com>
2018-01-06 21:21:27 -0500

    eslint semicolons and commas

--- src/components/SubwayMap.vue
+++ src/components/SubwayMap.vue
@@ -12,23 +12,24 @@ export default {
   props: ['colors'],
   created() {
     d3.json('./src/assets/mapSubway.json', (error, mapSubway) => {
-      var map = new spam.StaticCanvasMap({
+      const map = new spam.StaticCanvasMap({
         element: '.subway-map',
         data: [{
           features: topojson.feature(mapSubway, mapSubway.objects.mapSubway),
           static: {
-            paintfeature: (parameters, mapSubway) => {
-              const name = mapSubway.properties.name.substring(0, 1);
+            paintfeature: (parameters, mapSubway2) => {
+              const parametersScoped = parameters;
+              const name = mapSubway2.properties.name.substring(0, 1);
               const color = this.colors[name] ? this.colors[name].route_color : '000';
 
-              parameters.context.strokeStyle = `#${color}`;
-              parameters.context.stroke();
-            }
-          }
-        }]
-      })
-      map.init()
+              parametersScoped.context.strokeStyle = `#${color}`;
+              parametersScoped.context.stroke();
+            },
+          },
+        }],
+      });
+      map.init();
     });
-  }
-}
+  },
+};
 </script>
