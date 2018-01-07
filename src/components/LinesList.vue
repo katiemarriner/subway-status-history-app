a549392202a03e23c78ac5a3dcb3f35b6e66e97b
katiemarriner <kemarriner@gmail.com>
2018-01-06 21:23:02 -0500

    style initial dashboard view

--- src/components/LinesList.vue
+++ src/components/LinesList.vue
@@ -1,20 +1,44 @@
 <template>
   <div class="half">
-    <div class="trainList-container half">
-      <div
+    <div class="trainList-container">
+      <table-component
+          table-class="trainList-table"
+         :data="trains"
+         sort-by="route_short_name"
+         sort-order="asc"
+         :show-caption="false"
+         :show-filter="false"
+         @rowClick="getTrain"
+         >
+         <table-column
+          show="route_short_name"
+          label="Route"
+          :formatter="formatter">
+            <template slot-scope="obj">
+              <span
+                class="route-symbol"
+                :style="setStyle(obj.route_color)">{{ obj.route_short_name }}
+              </span>
+            </template>
+          </table-column>
+         <table-column
+          show="route_long_name"
+          label=""></table-column>
+     </table-component>
+      <!-- <div
         v-for="train in trains"
         class="trainList-row"
         :key="train.route_id">
         <router-link
           class="trainList-routeId"
           :to="{ name: 'line', params: { id: train.route_id }}">
-          {{ train.route_id }}
+          <LinesListRow></LinesListRow>
         </router-link>
-      </div>
+      </div> -->
     </div>
-    <div class="half">
-      <SubwayMap :colors="detail" />
-    </div>
+    <!-- <div class="half">
+      <subway-map :colors="detail" />
+    </div> -->
     <div class="trainDetail-container">
       <router-view
         :detail="detail">
@@ -24,6 +48,7 @@
 </template>
 
 <script>
+import LinesListRow from './LinesListRow';
 import SubwayMap from './SubwayMap';
 
 export default {
@@ -31,10 +56,29 @@ export default {
   props: ['trains', 'detail'],
   data() {
     return {
-
+      columns: [{
+        name: 'route_id',
+      }],
     };
   },
+  mounted() {
+
+  },
+  methods: {
+    formatter(routeId) {
+      return routeId;
+    },
+    getTrain(e) {
+      this.$router.push({ path: `/line/${e.data.route_id}` });
+    },
+    setStyle(color) {
+      return {
+        backgroundColor: `#${color}`,
+      };
+    },
+  },
   components: {
+    LinesListRow,
     SubwayMap,
   },
 };
